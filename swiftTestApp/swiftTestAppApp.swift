8564214573cfa54d8c6b29a26b2e7c65819b56dc
evan <evanmasiello@gmail.com>
2025-04-29 21:17:45 -0400

    Initial Commit

---
+++ swiftTestApp/swiftTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTestAppApp.swift
+//  swiftTestApp
+//
+//  Created by Evan Masiello on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftTestAppTests/swiftTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTestAppTests.swift
+//  swiftTestAppTests
+//
+//  Created by Evan Masiello on 4/29/25.
+//
+
+import Testing
+@testable import swiftTestApp
+
+struct swiftTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
